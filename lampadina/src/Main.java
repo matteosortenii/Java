3ef8895f6fc17ba27b4792fbd45c49031d221bfb
matteosortenii <matteosorrt@gmail.com>
2026-09-19 09:17:36 +0200

    creazione della classe lampadina

--- lampadina/src/Main.java
+++ lampadina/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("ciao mondo");
+        lampadina l;
+        l = new lampadina();
+        l.potenza = 30;
         }
 }
---
+++ lampadina/src/lampadina.java
@@ -0,0 +1,7 @@
+public class lampadina {
+    int potenza;
+    String colore;
+    int intensita;
+    boolean accesa;
+    String nome;
+}
