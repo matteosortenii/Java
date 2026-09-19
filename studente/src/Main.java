786eace00f5e15395f8c61febbfc2e93b1f601bf
matteosortenii <matteosorrt@gmail.com>
2026-09-19 09:35:38 +0200

    assegnazione dei campi alla classe studente

--- studente/src/Main.java
+++ studente/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        studente s;
+        s = new studente();
+        s.nome = "Matteo";
+        s.cognome = "Sorteni";
+        s.peso = (float) 57.5;
+        s.altezza = (float) 1.75;
     }
 }
