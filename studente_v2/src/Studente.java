cab88ced4df66596681a9a5ab971a08a24054b9a
matteosortenii <matteosorrt@gmail.com>
2026-09-26 08:58:57 +0200

    aggiunta verifica sulla correttezza dell'età, peso e altezza; aggiunta del metodo calcolaIndice (per calcolare il BMI di uno studente) e il metodo per stampare a video i dati dello studente

--- studente_v2/src/Studente.java
+++ studente_v2/src/Studente.java
@@ -2,12 +2,37 @@ public class Studente {
     private String nome;
     private String cognome;
     int eta;
-    float altezza;
+    double altezza;
+    double peso;
 
-    public Studente(String name, String surname, int age, float height){
-        name = nome;
-        surname = cognome;
-        eta = age;
-        altezza = height;
+    public Studente(String name, String surname, int age, double height, double wheight){
+        this.nome = " ";
+        this.cognome = " ";
+        if (this.eta <= 5) {
+            eta = 5;
+        } else this.eta = 0;
+        if (this.altezza < 0.8){
+            altezza = 0.8;
+        }
+        if (this.peso < 20){
+            this.peso = 20;
+        }
+
+    public String calcolaIndice(){
+        double BMI = peso / (altezza * altezza);
+        if (BMI < 18.5){
+            return "sottopeso";
+        } else if (BMI >= 18.5 && BMI <= 24.9){
+            return "normopeso";
+        } else if (BMI >= 25 && BMI <= 29.9){
+            return "sovrappeso";
+        } else return "obeso";
+    }
+
+    //override
+    public String toString(){
+        String s = "i dati dello studente sono: ";
+        s += this.nome + ", " + this.cognome +", " + this.eta + ", " + this.altezza;
+        return s;
     }
 }
