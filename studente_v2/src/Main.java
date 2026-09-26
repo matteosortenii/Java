6e636738d8d291b68c170a14f24b1c329db0525b
matteosortenii <matteosorrt@gmail.com>
2026-09-26 09:32:20 +0200

    creazione del menu nel main e del case 1

--- studente_v2/src/Main.java
+++ studente_v2/src/Main.java
@@ -3,17 +3,44 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Studente studente1 = new Studente(nome1, cognome1, eta1, altezza1, peso1);
-        Studente studente2 = new Studente(nome2, cognome2, eta2, altezza2, peso2);
+        Scanner input = new Scanner(System.in);
 
+        Studente studente1 = null;
+        Studente studente2 = null;
 
-        String nome;
-        String cognome;
-        int eta;
-        float altezza;
+        int scelta = 0;
 
-        Scanner sc = new Scanner(System.in);
-        nome = sc.nextLine();
-        cognome = sc.nextLine();
+        while (scelta !=4){
+            System.out.println("digitare 1 per inserie i dati dei 2 studenti");
+            System.out.println("digitare 2 per calcolare la massa corporea dei 2 studenti");
+            System.out.println("digitare 3 per stampare i dati dei 2 studenti");
+            System.out.println("digitare 4 per uscire");
+
+            scelta = input.nextInt();
+
+            switch (scelta){
+                case 1:
+                    System.out.println("inserire nome, cognome, eta, altezza e peso dello studente 1");
+                    String nome1 = input.next();
+                    String cognome1 = input.next();
+                    int eta1 = input.nextInt();
+                    double altezza1 = input.nextDouble();
+                    double peso1 = input.nextDouble();
+
+                    studente1 = new Studente(nome1, cognome1, eta1, altezza1, peso1);
+
+                    System.out.println("Inserire nome, cognome, eta, altezza e peso dello studente 2");
+
+                    String nome2 = input.next();
+                    String cognome2 = input.next();
+                    int eta2 = input.nextInt();
+                    double altezza2 = input.nextDouble();
+                    double peso2 = input.nextDouble();
+
+                    studente2 = new Studente(nome2, cognome2, eta2, altezza2, peso2);
+
+                    break;
+            }
+        }
     }
 }
